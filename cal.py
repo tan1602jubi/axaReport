d9e5b9c498ec064d56730188e06a6ad9756b63fd
sunnyyv <sunny@jubi.ai>
2020-01-17 13:44:59 +0530

    msg

--- cal.py
+++ cal.py
@@ -8,7 +8,7 @@ def getJourney(obj):
     elif 'Quote Gen Rollover' in list(obj.values()) or 'rolloverpolicy' in list(obj.values()) or 'renew policy from other insurance company':
          return 'rolloverBike'
     elif 'buy new policy' in list(obj.values()):
-         return 'buynew'
+         return 'buynewBike'
     else:
         return "unknown_Journey"
 
