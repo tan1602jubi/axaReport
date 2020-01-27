7688991754f3a758af6048c300ad4e93c67f78b7
tan1602jubi <tanmay@jubi.ai>
2020-01-27 12:44:33 +0530

    msg

--- cal.py
+++ cal.py
@@ -53,7 +53,7 @@ def getUser(obj):
         return "Unknown_Source"
 
 def quoteGenCount(obj):
-    if not obj["QUOTENUM"] == 'nan' and obj["QUOTENUM"] == 'NA':
+    if not obj["QUOTENUM"] == 'nan' and not obj["QUOTENUM"] == 'NA':
         return 1
     else:
         return 0
